042d8991017c41dc060265d8b771e4d9b0a4f5c6
willie <wwilliams@lyft.com>
2018-02-08 18:45:44 -0800

    Add non-generic training

--- run_tarnekatron.py
+++ run_tarnekatron.py
@@ -1,6 +1,6 @@
 from tarnekatron import Tarnekatron
 
 if __name__ == "__main__":
-    tarnekatron = Tarnekatron()
+    tarnekatron = Tarnekatron(weights_path="weights.hdf5")
 
-    print(tarnekatron.generate())
+    print(tarnekatron.generate(10))
--- tarnekatron.py
+++ tarnekatron.py
@@ -1,9 +1,20 @@
 from textgenrnn import textgenrnn
 
 class Tarnekatron:
-    def __init__(self):
-        self.textgen = textgenrnn()
+    def __init__(self, weights_path=None):
+        if weights_path is not None:
+            print("Loading previously trained weights")
+            self.textgen = textgenrnn(weights_path=weights_path)
+        else:
+            print("Creating intital model")
+            self.textgen = textgenrnn()
 
+
+    def train(self, file_path):
+        self.textgen.train_from_file(file_path)
+
+    def save(self, weights_path="weights.hdf5"):
+        self.textgen.save(weights_path=weights_path)
 
     def generate(self, lines=1):
         return self.textgen.generate(lines)
